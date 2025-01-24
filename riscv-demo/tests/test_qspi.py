1c1bcf24830721c5f3d8e9a6cb770ac8b2582dfc
gatecat <gatecat@ds0.me>
2025-01-24 11:25:44 +0100

    QSPI: Add a bypass mode for direct SPI IO
    Signed-off-by: gatecat <gatecat@ds0.me>

--- riscv-demo/tests/test_qspi.py
+++ riscv-demo/tests/test_qspi.py
@@ -15,6 +15,7 @@ class _QSPIFlashCommand(enum.Enum, shape=8):
     FastReadQuadOut     = 0x6B
     FastReadDualInOut   = 0xBB
     FastReadQuadInOut   = 0xEB
+    ReadID              = 0x9F
 
 
 class _MockFlash(wiring.Component):
@@ -40,16 +41,26 @@ class _MockFlash(wiring.Component):
         m.d.sync += self.i_octets.valid.eq(0) # default
 
         with m.FSM():
-            with m.State("IDLE"):
+            with m.State("Idle"):
                 with m.If(self.o_octets.valid):
-                    m.d.sync += [
-                        Assert(self.o_octets.p.chip == 1),
-                        Assert(self.o_octets.p.mode == QSPIMode.PutX1),
-                        command.eq(self.o_octets.p.data),
-                        address_count.eq(2)
-                    ]
-                    m.next = "ADDRESS"
-            with m.State("ADDRESS"):
+                    with m.If(self.o_octets.p.mode == QSPIMode.Dummy):
+                        m.next = "Idle"
+                    with m.Else():
+                        m.d.sync += [
+                            Assert(self.o_octets.p.chip == 1),
+                            Assert((self.o_octets.p.mode == QSPIMode.PutX1) | (self.o_octets.p.mode == QSPIMode.Swap)),
+                            command.eq(self.o_octets.p.data),
+                            address_count.eq(2)
+                        ]
+                        with m.If(self.o_octets.p.mode == QSPIMode.Swap):
+                            # send a dummy response to the swap
+                            m.d.sync += self.i_octets.valid.eq(1)
+                            m.d.sync += self.i_octets.p.data.eq(0xFF)
+                        with m.If(self.o_octets.p.data == _QSPIFlashCommand.ReadID):
+                            m.next = "Send-ID"
+                        with m.Else():
+                            m.next = "Get-Address"
+            with m.State("Get-Address"):
                 with m.If(self.o_octets.valid):
                     m.d.sync += [
                         Assert(self.o_octets.p.chip == 1),
@@ -58,11 +69,11 @@ class _MockFlash(wiring.Component):
                         address_count.eq(address_count - 1)
                     ]
                     with m.If(address_count == 0):
-                        m.next = "DATA"
-            with m.State("DATA"):
+                        m.next = "Send-Data"
+            with m.State("Send-Data"):
                 with m.If(self.o_octets.valid):
                     with m.If(self.o_octets.p.mode == QSPIMode.Dummy):
-                        m.next = "IDLE"
+                        m.next = "Idle"
                     with m.Else():
                         m.d.sync += [
                             Assert(self.o_octets.p.chip == 1),
@@ -72,7 +83,18 @@ class _MockFlash(wiring.Component):
                             self.i_octets.valid.eq(1),
                             address.eq(address + 1)
                         ]
-
+            with m.State("Send-ID"):
+                with m.If(self.o_octets.valid):
+                    with m.If(self.o_octets.p.mode == QSPIMode.Dummy):
+                        m.next = "Idle"
+                    with m.Else():
+                        m.d.sync += [
+                            Assert(self.o_octets.p.chip == 1),
+                            Assert(self.o_octets.p.mode == QSPIMode.Swap),
+                            self.i_octets.p.data.eq(C(0xEF4018, 24).word_select(address_count, 8)),
+                            self.i_octets.valid.eq(1),
+                            address_count.eq(address_count - 1)
+                        ]
         return m
 
 async def _wb_read(self, ctx, dut, addr, r_data):
@@ -87,6 +109,24 @@ async def _wb_read(self, ctx, dut, addr, r_data):
     ctx.set(dut.wb_bus.stb, 0)
     self.assertEqual(ctx.get(dut.wb_bus.dat_r), r_data)
 
+async def _csr_access(self, ctx, dut, addr, r_stb=0, r_data=0, w_stb=0, w_data=0):
+
+    ctx.set(dut.csr_bus.addr, addr)
+    ctx.set(dut.csr_bus.r_stb, r_stb)
+    ctx.set(dut.csr_bus.w_stb, w_stb)
+    ctx.set(dut.csr_bus.w_data, w_data)
+
+    await ctx.tick()
+
+    if r_stb:
+        self.assertEqual(ctx.get(dut.csr_bus.r_data), r_data)
+
+    ctx.set(dut.csr_bus.r_stb, 0)
+    ctx.set(dut.csr_bus.w_stb, 0)
+
+
+
+
 class QSPITestCase(unittest.TestCase):
     def test_sim(self):
         dut = WishboneQSPIFlashController(addr_width=24, data_width=32)
@@ -98,8 +138,37 @@ class QSPITestCase(unittest.TestCase):
 
         connect(m, dut.spi_bus, phy)
 
+        config_addr       = 0x000
+        raw_control_addr  = 0x004
+        raw_tx_data_addr  = 0x008
+        raw_rx_data_addr  = 0x00c
+
         async def testbench(ctx):
             await _wb_read(self, ctx, dut, 0x0, 0xa9a8abaa)
+            await _wb_read(self, ctx, dut, 0x4, 0xadacafae)
+
+            # in WB mode: bypass mode not ready
+            await _csr_access(self, ctx, dut, raw_control_addr, r_stb=1, r_data=0)
+            # enter bypass mode
+            await _csr_access(self, ctx, dut, config_addr, w_stb=1, w_data=1)
+            for _ in range(10): await ctx.tick()
+            # in bypass mode: bypass ready
+            await _csr_access(self, ctx, dut, raw_control_addr, r_stb=1, r_data=1)
+            # read ID command
+            await _csr_access(self, ctx, dut, raw_tx_data_addr, w_stb=1, w_data=_QSPIFlashCommand.ReadID)
+            for _ in range(4): await ctx.tick()
+            for byte in reversed(range(3)):
+                await _csr_access(self, ctx, dut, raw_tx_data_addr, w_stb=1, w_data=0xFF)
+                for _ in range(4): await ctx.tick()
+                await _csr_access(self, ctx, dut, raw_rx_data_addr, r_stb=1, r_data=((0xEF4018 >> (8 * byte)) & 0xFF))
+
+            # exit bypass mode
+            await _csr_access(self, ctx, dut, config_addr, w_stb=1, w_data=0)
+            for _ in range(10): await ctx.tick()
+            # in WB mode: bypass mode not ready
+            await _csr_access(self, ctx, dut, raw_control_addr, r_stb=1, r_data=0)
+
+            # check that WB functions again
             await _wb_read(self, ctx, dut, 0x4, 0xadacafae)
 
         sim = Simulator(m)
@@ -107,4 +176,3 @@ class QSPITestCase(unittest.TestCase):
         sim.add_testbench(testbench)
         with sim.write_vcd(vcd_file="test_qspi.vcd"):
             sim.run()
-
